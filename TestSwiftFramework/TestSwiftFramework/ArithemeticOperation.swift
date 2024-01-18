171653310b86c0f0a82c1f1360a1623114f427d7
Kishore <Kishorechandru.Muthusamy@agilysys.com>
2024-01-18 17:15:52 +0530

    missed the commited files

--- TestSwiftFramework/TestSwiftFramework/ArithemeticOperation.swift
+++ TestSwiftFramework/TestSwiftFramework/ArithemeticOperation.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+public class ArithemeticOperation{
+    public init(){
+        
+    }
+    
+    public func add()->Int{
+        return 3
+    }
+    
+    public func sub()->Int{
+        return 1
+    }
+    
+}
